49e18c6c3f413757104d5f527dd59833ef586190
Ayo <Ayo.meshe@gmail.com>
2023-12-29 23:15:56 +0000

    person pill design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,33 @@
 import "./Hero.css";
 import React from "react";
 import EmailBox from "../EmailBox/EmailBox";
+import { HeroData } from "@/src/utils/data";
 
 const Hero = () => {
   return (
     <div className="h-wrapper">
       <div className="container">
         <div className="h-container">
-          <div className="h-left">Hero section</div>
+          <div className="h-left">
+            <div className="image-row">
+              {HeroData.slice(0, 3).map((person, i) => (
+                <div className="person-pill" key={i}>
+                  <div className="person-pill-bg">
+                    <img src={person.src} alt="person" />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="image-row">
+              {HeroData.slice(3, 6).map((person, i) => (
+                <div className="person-pill" key={i}>
+                  <div className="person-pill-bg">
+                    <img src={person.src} alt="person" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
 
           <div className="h-right">
             <div className="h-title">
