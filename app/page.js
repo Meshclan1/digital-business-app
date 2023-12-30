1799ef32fc858e0fbb93e522b5d1e1a1f471e715
Ayo <Ayo.meshe@gmail.com>
2023-12-30 18:20:42 +0000

    setting branding video animation

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Navbar from "@/src/components/Navbar/Navbar";
 import Hero from "@/src/components/Hero/Hero";
 import "./page.css";
+import BrandingVideo from "@/src/components/BrandingVideo/BrandingVideo";
 
 export default function Home() {
   return (
     <div className="app">
       <Navbar />
       <Hero />
+      <BrandingVideo />
     </div>
   );
 }
---
+++ src/components/BrandingVideo/BrandingVideo.jsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useRef } from "react";
+import "./BrandingVideo.css";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+const BrandingVideo = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end end"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  return (
+    <div className="bv-container">
+      <div className="container">
+        <motion.video
+          className="bv-video"
+          ref={ref}
+          loop
+          muted
+          autoPlay
+          controls=""
+          style={{ scale }}
+        >
+          <source src="/video.mp4" type="video/mp4" />
+        </motion.video>
+      </div>
+    </div>
+  );
+};
+
+export default BrandingVideo;
